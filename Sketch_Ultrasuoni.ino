9255e497c6ab48ba72a35d11a5478cabc7ddc4b3
Gabrieleiurlaro <giurlaro99@gmail.com>
2016-09-18 14:06:29 +0200

    Add files via upload

---
+++ Sketch_Ultrasuoni.ino
@@ -0,0 +1,28 @@
+const int triggerPort = 10;//imposto la porta del Trigger al pin 10
+const int echoPort = 11;//imposto la porta dell'echo al pin 11
+long t,distanza;
+void setup() {
+  Serial.begin(9600);
+  pinMode(triggerPort, OUTPUT);
+  pinMode(echoPort, INPUT);
+  Serial.print( "Sensore Ultrasuoni: ");
+}
+ 
+void loop() {
+  misuradistanza();
+  Serial.print("distanza: ");
+  Serial.println(distanza);
+}
+
+void misuradistanza(){
+  //porta bassa l'uscita del trigger
+  digitalWrite( triggerPort, LOW );
+  //invia un impulso di 10microsec su trigger
+  digitalWrite( triggerPort, HIGH );
+  delayMicroseconds( 10 );
+  digitalWrite( triggerPort, LOW );
+ 
+   t = pulseIn( echoPort, HIGH );
+   distanza = t/58;
+}
+
